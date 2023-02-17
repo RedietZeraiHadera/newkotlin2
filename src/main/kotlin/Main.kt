db59082a85cb46dcefa317da30c3702b928d19ac
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-02-17 11:52:28 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+val index = "akirachix"
+    println(index[0] + ""+index[2]+""+index[3])
+
+
+
+    val x = "Rediet"
+    val y = 22
+    val greeting = "Hi, My name is " + x + " and I am " + y + "years old."
+    println(greeting)
+
+    val str = "www.akirachix.com"
+    println(str.length)
+
+    var myName = "Rediet"
+    if (myName.equals("Rediet"))
+        println("That's me!")
+    else{
+        println("I don't know who that is")
+    }
+
+}
